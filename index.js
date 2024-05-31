29d32d1100c7edeb213e327299548e2f43770fcc
Nell5 <neeennever@gmail.com>
2024-05-31 15:42:25 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+let a = +prompt("son tanlang")
+
+let b = +prompt("2 sonni tanlang")
+
+let c = +prompt(" 1.+\n 2.-\n 3.%\n 4.*\n")
+
+if (c == 1) {
+    alert(a + b)
+} else if (c == 2) {
+    alert(a - b)
+} else if (c == 3) {
+    alert(a % b)
+} else if (c == 4) {
+    alert(a * b)
+} else {
+    alert("bunday operatsiya mavjud emas")
+}
+
+
